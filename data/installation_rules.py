23196cc187438999903f8ed1a28cf817efe28f26
CWisnu <cahyadisutoitec@gmail.com>
2026-06-29 15:08:34 +0700

    Create installation_rules.py

---
+++ data/installation_rules.py
@@ -0,0 +1,39 @@
+installation_rules = {
+
+    "Tee": {
+        "upstream": 15,
+        "downstream": 5,
+        "image": "tee.png"
+    },
+
+    "90° Elbow": {
+        "upstream": 20,
+        "downstream": 5,
+        "image": "elbow90.png"
+    },
+
+    "2 × 90° Elbow": {
+        "upstream": 35,
+        "downstream": 5,
+        "image": "double_elbow.png"
+    },
+
+    "Reducer": {
+        "upstream": 20,
+        "downstream": 5,
+        "image": "reducer.png"
+    },
+
+    "Flange": {
+        "upstream": 10,
+        "downstream": 5,
+        "image": "flange.png"
+    },
+
+    "Two Elbows on Different Plane": {
+        "upstream": 40,
+        "downstream": 5,
+        "image": "two_plane_elbow.png"
+    }
+
+}
