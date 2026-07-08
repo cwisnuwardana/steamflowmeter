67500ac280eb0e286bb930b4da8a62fe18d8f203
CWisnu <cahyadisutoitec@gmail.com>
2026-07-09 05:49:21 +0700

    Update my_footer.py

--- my_footer.py
+++ my_footer.py
@@ -1,1 +1,23 @@
+import streamlit as st
 
+
+def show_my_footer():
+
+    st.divider()
+
+    st.markdown(
+        """
+        <div style="
+            text-align:center;
+            color:#666666;
+            font-size:12px;
+            padding:10px;
+        ">
+        SUTO iTEC Indonesia<br>
+        Steam Flowmeter S435 Assistant<br>
+        Version 1.0<br> 
+        Created by Cahyadi Wisnu Wardana. MM
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
