8b942af9b29bb15c5434a5097d8d86001f5d8e3e
CWisnu <cahyadisutoitec@gmail.com>
2026-07-09 09:03:12 +0700

    Update evaluation_engine.py

--- utils/evaluation_engine.py
+++ utils/evaluation_engine.py
@@ -3,58 +3,79 @@ from utils.hydraulic_assessment import hydraulic_assessment
 from utils.pipe_lookup import get_pipe_data
 from utils.installation_rules import installation_rules
 
+
 def evaluate_pipe(
 
     result,
-
     existing_dn,
-
     selected_dn,
-
     pressure,
-
     schedule,
-
     disturbance
 
 ):
 
+    # =====================================
+    # PIPE OPTIMIZATION RESULT
+    # =====================================
+
     optimization = result["pipe_optimization"]
 
     best = optimization.iloc[0]
-    
+
     selected = optimization[
         optimization["DN"] == selected_dn
     ].iloc[0]
 
+    # =====================================
+    # PIPE DATA
+    # =====================================
+
     pipe = get_pipe_data(
+
         selected["DN"],
+
         schedule
+
     )
 
-    print("Pipe Data :", pipe)
-    print("========================")
+    pipe_id = pipe["id"]
+
+    # =====================================
+    # INSTALLATION RULE
+    # =====================================
+
     rule = installation_rules[disturbance]
-    
+
     upstream_D = rule["upstream"]
-    
+
     downstream_D = rule["downstream"]
-    
-    required_upstream = pipe["id"] * upstream_D
-    
-    required_downstream = pipe["id"] * downstream_D
-        
+
+    required_upstream = pipe_id * upstream_D
+
+    required_downstream = pipe_id * downstream_D
+
+    # =====================================
+    # DESIGN SPOOL
+    # =====================================
+
     spool = design_spool(
-    
+
         existing_dn,
-    
+
         selected["DN"],
-    
+
         required_upstream,
-    
-        required_downstream
-    
+
+        required_downstream,
+
+        schedule
+
     )
+
+    # =====================================
+    # HYDRAULIC
+    # =====================================
 
     hydraulic = hydraulic_assessment(
 
@@ -62,28 +83,40 @@ def evaluate_pipe(
 
         pressure_drop_bar_per_m=selected["Pressure Drop (bar/m)"],
 
-        spool_length_mm=spool["total_length"]
+        spool_length_mm=spool["fabrication_length"]
 
     )
+
+    # =====================================
+    # VELOCITY STATUS
+    # =====================================
 
     velocity = selected["Velocity (m/s)"]
 
     if 10 <= velocity <= 35:
+
         velocity_note = "ideal"
 
     elif 5 <= velocity < 10:
+
         velocity_note = "acceptable"
 
     elif 35 < velocity <= 45:
+
         velocity_note = "slightly high"
 
     else:
+
         velocity_note = "outside the recommended range"
 
+    # =====================================
+    # RETURN
+    # =====================================
+
     return {
-        
+
         "best": best,
-        
+
         "selected": selected,
 
         "spool": spool,
